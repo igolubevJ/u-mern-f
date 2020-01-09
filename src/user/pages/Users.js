7eb574872f6e7e757625708e3c7c8f9dd109369a
igolubev <igolubevapp@gmail.com>
2020-01-09 05:45:31 +0300

    Crate UsersList & UserItem components. Create UIElement Card & Avatar component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Users = () => (
-  <h2>Users works</h2>
-)
+import UsersList from '../components/UsersList';
+
+const Users = () => {
+  const USERS = [
+    { 
+      id: 'u1',
+      name: 'John Doe',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv3rrr58HrON4U10CMC2tXrpmfQE8OhpIa76Zgn0GHdHFqU6u-&s'
+    }
+  ];
+
+  return <UsersList items={ USERS } />
+}
 
 export default Users;
